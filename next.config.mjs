1a30cb73edfd4365b790678b29ea5cb45ff0232a
Joseph John <chatwithjohnjoseph@gmail.com>
2024-08-19 23:10:05 +0100

    Added asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 
+const repo = "quince-davis-ministries";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig = {
-  basePath: "/quince-davis-ministries",
+  basePath,
+  assetPrefix,
   output: "export",
   reactStrictMode: true,
   images: { unoptimized: true },
