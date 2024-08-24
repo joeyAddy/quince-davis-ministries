df4a5da90db88f8c0b590921b6da992ade932843
Joseph John <chatwithjohnjoseph@gmail.com>
2024-08-24 12:53:14 +0100

    Removed unsused imports

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,8 @@
-import About from "@/components/sections/About";
 import ChrismasMoments from "@/components/sections/ChrismasMoments";
 import Donate from "@/components/sections/Donate";
 import Events from "@/components/sections/Events";
 import Footer from "@/components/sections/Footer";
 import Hero from "@/components/sections/Hero";
-import OurTeam from "@/components/sections/OurTeam";
-import Services from "@/components/sections/Services";
 import Testimonials from "@/components/sections/Testimonials";
 
 export default function Home() {
